d21c547cf4f7ca5518e3ba95e72ecb4aa2540b2c
Donovan Moini <donovanmoini@gmail.com>
2020-04-09 17:18:14 -0700

    Beginning code of CLI

---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// TODO: create commmand line args parser and usage instructions
+// yargs, command-line-args, meow, args
+
+console.log(process.argv);
---
+++ util/executeCommand.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const { authorize } = require("./googleAuth");
+
+exports.executeCommand = (command) => {
+  // Load client secrets from a local file.
+  fs.readFile("credentials.json", (err, content) => {
+    if (err) return console.log("Error loading client secret file:", err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content), command);
+  });
+};
